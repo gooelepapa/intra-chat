ac64b4e3df52780af90b601c79cd8b1748782770
Cose <gooelepapa@gmail.com>
2025-07-24 17:20:54 +0800

    Feat: 把路徑參數化
    Info:
    在 `src/config.py` 中定義了
    - CRAWLER_DATA_ROOT: 爬蟲數據根目錄
    - INGESTED_ARTICLES: 存已經被加入RAG資料庫的文章

--- src/config.py
+++ src/config.py
@@ -21,8 +21,22 @@ class Configuration(BaseSettings):
     QDRANT_URL: str = "http://localhost:6333"
     QDRANT_API_KEY: str = None  # Optional API key for Qdrant
     QDRANT_COLLECTION: str = "rag_collection"
+    # Crawler arguments
+    CRAWLER_DATA_ROOT: str = "data/articles"
+    # Ingested arguments
+    INGESTED_ARTICLES: str = "ingested_articles"
 
     model_config = SettingsConfigDict(env_file=".env")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Create folder if it does not exist
+        if not os.path.exists(self.CRAWLER_DATA_ROOT):
+            os.makedirs(self.CRAWLER_DATA_ROOT)
+            print(f"Created directory: {self.CRAWLER_DATA_ROOT}")
+        if not os.path.exists(self.INGESTED_ARTICLES):
+            os.makedirs(self.INGESTED_ARTICLES)
+            print(f"Created directory: {self.INGESTED_ARTICLES}")
 
 
 configuration = Configuration()
